328910abaa44da43d777c4af8dbe942af02ac4d3
Anton Fagerholm <anton.fagerholm@gmail.com>
2020-03-28 10:10:57 +0200

    Unicafe done

--- Osa_1/unicafe/src/index.js
+++ Osa_1/unicafe/src/index.js
@@ -33,7 +33,6 @@ const {good, neutral, bad} = props;
 }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
