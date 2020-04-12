c0021226d889731bfe3743f404f65d7247fe8e34
Anton Fagerholm <anton.fagerholm@gmail.com>
2020-04-12 14:05:08 +0300

    step 2.5 done

---
+++ Osa_2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Course = (props) => {
+  return (
+    <div>
+      {props.course.map((course, i) =>
+      <div key={i}>
+        <Header course={props.course[i].name}/>
+        <Content part={props.course[i].parts}/>
+      </div>
+      )}
+    </div>
+  )
+}
+
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = (props) => {
+  return (
+    <>
+       {props.part.map((part, i)=>
+       <p key={i}>
+         {props.part[i].name} {props.part[i].exercises}
+       </p>
+       )}
+    </>
+  )
+}
+
+const Content = ({ part }) => {
+  const total = part.reduce((total, {exercises})=> total + exercises, 0)
+    return(
+     <>
+      <Part part={part}/>
+      <b>Total of exercises {total}</b>
+    </>
+   )
+}
+export default Course
