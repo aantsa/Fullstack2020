eaf7e8ab93e47a0e8a3ade9e89a4c51761f1f4f8
Anton Fagerholm <anton.fagerholm@gmail.com>
2022-08-23 20:01:51 +0300

    heroku baseurl added

--- Osa_2/puhelinluettelo/src/services/notes.js
+++ Osa_2/puhelinluettelo/src/services/notes.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const baseUrl = "http://localhost:3002/persons";
+const herokuUrl = "https://infinite-plateau-87631.herokuapp.com/api/persons";
 
 const getAll = () => {
   return axios.get(baseUrl)
