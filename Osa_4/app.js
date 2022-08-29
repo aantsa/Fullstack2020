25d0275388c9f1b1fc5118464c3c822784b33e6f
Anton Fagerholm <anton.fagerholm@gmail.com>
2022-08-29 19:30:43 +0300

    part 4 initialized and extracted files

---
+++ Osa_4/app.js
@@ -0,0 +1,30 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const router = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+logger.info("trying to connect to the database...");
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("database connection established");
+  })
+  .catch((error) => {
+    logger.error("error occured while trying to create a database connection: " + error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.reqLogger);
+
+app.use("/api/blogs", router);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
