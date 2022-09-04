4e225ce443130bfbcc765950f28d81fa16a6db31
Anton Fagerholm <anton.fagerholm@gmail.com>
2022-09-04 10:32:05 +0300

    4.13 and 4.14

--- Osa_4/controllers/blogs.js
+++ Osa_4/controllers/blogs.js
@@ -13,4 +13,17 @@ router.post("/", async (req, res) => {
   res.status(201).json(savedBlog.toJSON());
 });
 
+router.delete("/:id", async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
+
+router.put("/:id", async (req, res) => {
+  const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  blog ? res.status(200).json(blog.toJSON()) : res.status(400).end();
+});
+
 module.exports = router;
--- Osa_4/tests/blog_api.test.js
+++ Osa_4/tests/blog_api.test.js
@@ -10,76 +10,118 @@ beforeEach(async () => {
   await Blog.insertMany(blogs);
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("fetching blog/blogs", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("blogs identifier named as id", async () => {
+    const res = await api.get("/api/blogs");
+
+    const ids = res.body.map((blog) => blog.id);
+
+    for (const id of ids) {
+      expect(id).toBeDefined();
+    }
+  });
 });
 
-test("blogs identifier named as id", async () => {
-  const res = await api.get("/api/blogs");
+describe("post a blog", () => {
+  test("new valid blog test ", async () => {
+    const blog = {
+      title: "A Blog",
+      author: "Eventful Blogger",
+      url: "https://fullstackopen.com/",
+      likes: 4,
+    };
 
-  const ids = res.body.map((blog) => blog.id);
+    await api
+      .post("/api/blogs")
+      .send(blog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  for (const id of ids) {
-    expect(id).toBeDefined();
-  }
+    const blogListAfter = await Blog.find({});
+    const afterLength = blogListAfter.map((b) => b.toJSON());
+
+    expect(afterLength).toHaveLength(blogs.length + 1);
+
+    const addedBlog = blogListAfter.map((blog) => blog.title);
+    expect(addedBlog).toContain("A Blog");
+  });
+
+  test("set likes to 0 as default", async () => {
+    const blog = {
+      title: "A blog with 0 likes",
+      author: "Joyful Blogger",
+      url: "https://fullstackopen.com/",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(blog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogListAfter = await Blog.find({});
+    const afterLength = blogListAfter.map((b) => b.toJSON());
+
+    expect(afterLength).toHaveLength(blogs.length + 1);
+    expect(afterLength[afterLength.length - 1].likes).toEqual(0);
+  });
+
+  test("status 400 if title or url missing", async () => {
+    const blog = {
+      author: "Playful blogger",
+    };
+
+    await api.post("/api/blogs").send(blog).expect(400);
+
+    const blogListAfter = await Blog.find({});
+    const afterLength = blogListAfter.map((b) => b.toJSON());
+
+    expect(afterLength).toHaveLength(blogs.length);
+  });
 });
 
-test("new valid blog test ", async () => {
-  const blog = {
-    title: "A Blog",
-    author: "Eventful Blogger",
-    url: "https://fullstackopen.com/",
-    likes: 4,
-  };
+describe("delete blog", () => {
+  test("delete blog if status code 204", async () => {
+    const blogs = await Blog.find({});
+    const blogList = blogs.map((blog) => blog.toJSON());
 
-  await api
-    .post("/api/blogs")
-    .send(blog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    const blogDelete = blogList[0];
 
-  const blogListAfter = await Blog.find({});
-  const afterLength = blogListAfter.map((b) => b.toJSON());
-  expect(afterLength).toHaveLength(blogs.length + 1);
+    await api.delete(`/api/blogs/${blogDelete.id}`).expect(204);
 
-  const addedBlog = blogListAfter.map((blog) => blog.title);
-  expect(addedBlog).toContain("A Blog");
+    const blogListAfter = await Blog.find({});
+    const afterLength = blogListAfter.map((b) => b.toJSON());
+    expect(afterLength).toHaveLength(blogs.length - 1);
+
+    expect(afterLength.map((r) => r.id)).not.toContain(blogDelete.id);
+  });
 });
 
-test("set likes to 0 as default", async () => {
-  const blog = {
-    title: "A blog with 0 likes",
-    author: "Joyful Blogger",
-    url: "https://fullstackopen.com/",
-  };
+describe("update blog", () => {
+  test("200 if updated, othewise 404", async () => {
+    const blogs = await Blog.find({});
+    const blogList = blogs.map((blog) => blog.toJSON());
 
-  await api
-    .post("/api/blogs")
-    .send(blog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    const blogUpdate = blogList[0];
 
-  const blogListAfter = await Blog.find({});
-  const afterLength = blogListAfter.map((b) => b.toJSON());
+    await api.put(`/api/blogs/${blogUpdate.id}`).send({ likes: 1 }).expect(200);
 
-  expect(afterLength).toHaveLength(blogs.length + 1);
-  expect(afterLength[afterLength.length - 1].likes).toEqual(0);
-});
+    const blogListAfter = await Blog.find({});
+    const afterLength = blogListAfter.map((b) => b.toJSON());
+    const updateBlog = afterLength[0];
 
-test("status 400 if title or url missing", async () => {
-  const blog = {
-    author: 'Playful blogger',
-  };
+    expect(afterLength).toHaveLength(blogs.length);
 
-  await api.post("/api/blogs").send(blog).expect(400);
-  
-  const blogListAfter = await Blog.find({});
-  const afterLength = blogListAfter.map((b) => b.toJSON());
+    expect(updateBlog.likes).toBe(1);
 
-  expect(afterLength).toHaveLength(blogs.length);
+  });
 });
 
 afterAll(() => {
