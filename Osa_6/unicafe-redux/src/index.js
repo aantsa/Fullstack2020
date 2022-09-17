219870ae106add3d50a5766273f43c21e07e5b9e
Anton Fagerholm <anton.fagerholm@gmail.com>
2022-09-17 10:31:14 +0300

    tasks 6.1-6.2 done

---
+++ Osa_6/unicafe-redux/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+const store = createStore(reducer);
+
+const App = () => {
+  const increment = (value) => {
+    store.dispatch({ type: value });
+  };
+
+  return (
+    <div>
+      <button onClick={() => increment("GOOD")}>good</button>
+      <button onClick={() => increment("OK")}>ok</button>
+      <button onClick={() => increment("BAD")}>bad</button>
+      <button onClick={() => increment("ZERO")}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
